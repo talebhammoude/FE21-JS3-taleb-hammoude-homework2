6a8f379fd1953e5f3c607a2c33774f82e1c49112
Taleb Hammoudé <taleb.hammoude@outlook.com>
2022-11-05 14:26:57 +0100

    update

--- homework2/src/Components/CategoryButtons/index.js
+++ homework2/src/Components/CategoryButtons/index.js
@@ -6,17 +6,31 @@ function CategoryButtons(props) {
 
     const setCats = new Set(props.theCats);
     const setCatsArray = [...setCats];
-    
+
+
+
+    const handleClickOnCat = (e)=> {
+        // e.target.style['border-width'] = "7px";
+        
+    }
+
+    const handleClickOnAll = (e)=> {
+        
+        
+    }
+
+
+
 
   return (
     
     <div className="catButtons">
 
-        <Button variant="secondary" >ALL</Button>{'  '}
+        <Button variant="secondary" onClick={handleClickOnAll}>ALL</Button>{'  '}
           {setCatsArray.map((e) => {
             return (
               <span>
-                 <Button variant="warning">{e}</Button>{'  '}
+                 <Button variant="warning" onClick={handleClickOnCat} id="catBtn">{e}</Button>{'  '}
               </span>
             );
           })}
